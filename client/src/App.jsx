cf64bf87285a11756f8e8816173435e1feecca93
Jayson Baker <bakej07@gmail.com>
2023-08-10 11:42:39 -0500

    Added file structure for nav bar and made files read as jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,11 @@
-import React, { useState } from 'react'
-import Navbar from './components/navbar'
-import Homepage from './components/homepage'
-import Profile from './components/profile'
-import MyGames from './components/myGames'
-import MyPosts from './components/myPosts'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Nav from './components/Nav'
 
 function App() {
-  const [pages] = useState([
-    {
-      name:'home page'
-    },
-    {
-      name: 'profile'
-    },
-    {
-      name: 'my games'
-    },
-    {
-      name: 'my posts'
-    }
-  ])
-  const [currentPage, setCurrentPage] = useState(pages[0])
-
   return (
     <>
-      <div>
-        <Navbar>
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-
-        </Navbar>
-        <main>
-          {(()=>{
-            switch(currentPage.name){
-              case 'profile':
-                return<Profile />
-              case 'my games':
-                return <MyGames />
-              case 'MyPosts':
-                return <MyPosts />
-              default:
-                return <Homepage />
-            }
-          })}
-           
-        </main>
-      </div>
+      <Nav />
+      <Outlet />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 
+import App from './App.jsx'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'profile/:id',
+        element: <ProfilePage />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
