8da5f21bb27dbe3f29e854f59f8e64c5da78b5ec
frostyfaust <55112932+frostyfaust@users.noreply.github.com>
2023-08-09 19:40:47 -0500

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,17 +1,16 @@
 import React from "react";
+import PageNav from "./pageNav"
 
 function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className='container-fluid'>
-                <span className='navbar-brand'
+        <nav className="">
+            <div className=''>
+                <span className=''
                     onClick={() => { props.setCurrentPage(props.pages[0]) }}>
-                    Preston Fassbender
+                    Gamers Guild
                 </span>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <Navigation {...props}></Navigation>
+    
+                <PageNav {...props}></Navigation>
             </div>
         </nav>
     )
