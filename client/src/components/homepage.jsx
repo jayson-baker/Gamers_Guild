09cf884e5580177416dfe98f40a12f30355f574c
frostyfaust <55112932+frostyfaust@users.noreply.github.com>
2023-08-09 21:37:59 -0500

    created outlet router and name changes

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Homepage(){
+function HomePage(){
 
     return(
     <div>
@@ -9,4 +9,4 @@ function Homepage(){
     )
 }
 
-export default Homepage
+export default HomePage
