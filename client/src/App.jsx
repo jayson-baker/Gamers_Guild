a4937bb4291129a2800116259d59ee9d02d9ff08
Jayson Baker <bakej07@gmail.com>
2023-08-11 14:25:49 -0500

    Example game card done.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import NavBar from './components/navbar'
+import HomePage from "./components/homepage"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <HomePage />
     </div>
   )
 }
