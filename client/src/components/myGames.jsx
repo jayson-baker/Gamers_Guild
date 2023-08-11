e47ae8e2d16c5037517dcaa0c4dd3746b01e82bc
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-11 16:16:17 -0500

    gettin games data

--- client/src/components/myGames.jsx
+++ client/src/components/myGames.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 
 function MyGames(){
 
+const [games, setGames] = useState([]);
+const url = 'http://localhost:3000/';
+
+useEffect(() => {
+    const getGames = async () => {
+        try {
+        const response = await axios.get(`${url}past`);
+        const allGames = response.data.games.allGames;
+        setGames(allGames)
+        } catch (error) {
+            console.error('Error getting games', error);
+        }
+    };
+
+    getGames();
+}, [])
+
 }
 
-export default MyGames
+
+export default MyGames;
