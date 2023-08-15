a6cc1252bce8061ef18a0cd34a707c02f7a61f95
Jayson Baker <bakej07@gmail.com>
2023-08-15 10:25:53 -0500

    WIP login modal and login functionality

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -17,7 +17,17 @@ const Navbar = () => {
       {/* NavBar Buttons */}
       <ul className='hidden md:flex md:items-baseline'>
         <li className="mr-4"><Link to="/">Home</Link></li>
-        <li><Link to="/">Log In</Link></li>
+        <li>
+          <button className="btn" onClick={()=>window.my_modal_3.showModal()}>Login</button>
+          <dialog id="my_modal_3" className="modal">
+            <form method="dialog" className="modal-box">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">Press ESC key or click on ✕ button to close</p>
+            </form>
+          </dialog>
+      
+        </li>
       </ul>
 
       {/* Hamburger */}
