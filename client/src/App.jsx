a415bd9f5225dc89ccc3a9783bed213e23a82cd5
Jayson Baker <bakej07@gmail.com>
2023-08-16 11:03:54 -0500

    Login now makes it to the twitchValidation. Implemented Apollo on client.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,36 @@
 import { Outlet } from "react-router-dom";
 import Nav from './components/navbar'
 import SideBar from "./components/SideBar";
+import { ApolloProvider, InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <>
-    <Nav />
+    <ApolloProvider client={client}>
+      <Nav />
       <SideBar />
       <Outlet />
-
+    </ApolloProvider>
     </>
   )
 }
