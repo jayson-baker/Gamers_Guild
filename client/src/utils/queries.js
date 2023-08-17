c367e4a3404ce053583049b8780a045db5df2fd4
Jayson Baker <bakej07@gmail.com>
2023-08-16 21:09:50 -0500

    Users can now signup, login, and logout. Complete

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,16 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
   {
     user {
       usernname
-      posts{
+      posts {
         _id
         title
         createdAt
         game {
-            _id
-            name
+          _id
+          name
         }
       }
       games {
@@ -25,59 +25,59 @@ export const QUERY_USER = gql`
 export const QUERY_ALL_POSTS = gql`
   {
     posts {
+      _id
+      username
+      title
+      createdAt
+      game {
         _id
-        username
-        title
-        createdAt
-        game{
-            _id
-            name
-            image
-        }
+        name
+        image
+      }
     }
   }
 `;
 export const QUERY_ALL_Games = gql`
   {
     games {
-        _id
-         name
-    }    
+      _id
+      name
+    }
   }
 `;
 
 export const QUERY_POST = gql`
-query geAPost($posts: [ID]!){
- getPost(post: $posts){
-    _id
-    username
-    title
-    createdAt
-    game{
+  query getPost($posts: [ID]!) {
+    getPost(post: $posts) {
+      _id
+      username
+      title
+      createdAt
+      game {
         _id
         name
-    }
-    replies{
+      }
+      replies {
         _id
         text
         username
         createdAt
+      }
     }
- }
-}
-`
+  }
+`;
 
 export const QUERY_POSTS_BY_GAME = gql`
   query getPosts($game: ID) {
     products(game: $game) {
+      _id
+      username
+      title
+      createdAt
+      game {
         _id
-        username
-        title
-        createdAt
-        game{
-            _id
-            name
-        }
+        name
+      }
     }
   }
 `;
